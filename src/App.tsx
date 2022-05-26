36e0f4702ff4243f6ddd03097ef5faa7b63d26e3
matheuspython <ruivorobado@gmail.com>
2022-05-25 22:34:53 -0300

    iniciando main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from './components/Header'
+import { Main } from './components/Main'
 import { Global } from './global/styles'
 export const App:React.FC = () => {
   return (
-    <div className="App">
+    <>
       <Global />
       <Header />
-    </div>
+      <Main />
+    </>
   )
 }
 
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export const Main: React.FC = () => {
+  return (
+    <Container>
+      main
+    </Container>
+  )
+}
